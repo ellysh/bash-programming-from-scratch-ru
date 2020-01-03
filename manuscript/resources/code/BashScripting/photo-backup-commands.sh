4179059fbba7aa1b1cedf54f5cae874354b284f1
Ilya Shpigor <petrsum@gmail.com>
2020-01-03 09:22:29 +0100

    Move listings to the separate files

---
+++ manuscript/resources/code/BashScripting/photo-backup-commands.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bsdtar -cjf ~/photo.tar.bz2 ~/photo && echo "bsdtar - OK" > results.txt || ! echo "bsdtar - FAILS" > results.txt
+cp -f ~/photo.tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
---
+++ manuscript/resources/code/BashScripting/photo-backup-exit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bsdtar -cjf ~/photo.tar.bz2 ~/photo && echo "bsdtar - OK" > results.txt || (echo "bsdtar - FAILS" > results.txt && exit 1)
+cp -f ~/photo.tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
