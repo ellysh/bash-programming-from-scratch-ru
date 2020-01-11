03b5de8185f7d6ee4fd20ea6086e481f9e473896
Ilya Shpigor <petrsum@gmail.com>
2020-01-11 16:40:28 +0100

    Fix a mistake with the "set -e" option

--- manuscript/resources/code/BashScripting/make-backup.sh
+++ manuscript/resources/code/BashScripting/make-backup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-set -e
-bsdtar -cjf "$1".tar.bz2 "$@" && echo "bsdtar - OK" > results.txt || ! echo "bsdtar - FAILS" > results.txt
+bsdtar -cjf "$1".tar.bz2 "$@" && echo "bsdtar - OK" > results.txt || (echo "bsdtar - FAILS" > results.txt && exit 1)
 mv -f "$1".tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
--- manuscript/resources/code/BashScripting/photo-backup-parameter-path.sh
+++ manuscript/resources/code/BashScripting/photo-backup-parameter-path.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-set -e
-bsdtar -cjf ~/photo.tar.bz2 "$1" && echo "bsdtar - OK" > results.txt || ! echo "bsdtar - FAILS" > results.txt
+bsdtar -cjf ~/photo.tar.bz2 "$1" && echo "bsdtar - OK" > results.txt || (echo "bsdtar - FAILS" > results.txt && exit 1)
 cp -f ~/photo.tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
