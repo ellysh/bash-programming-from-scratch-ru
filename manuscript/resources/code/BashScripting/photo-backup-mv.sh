6fa7fa9c69f16fcf3d18d9a8e3b4cc11dde18a5b
Ilya Shpigor <petrsum@gmail.com>
2020-01-12 10:11:52 +0100

    Fix a mistake with usage the "exit" command from the subshell

--- manuscript/resources/code/BashScripting/photo-backup-mv.sh
+++ manuscript/resources/code/BashScripting/photo-backup-mv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bsdtar -cjf "$1".tar.bz2 "$1" && echo "bsdtar - OK" > results.txt || (echo "bsdtar - FAILS" > results.txt && exit 1)
+bsdtar -cjf "$1".tar.bz2 "$1" && echo "bsdtar - OK" > results.txt || { echo "bsdtar - FAILS" > results.txt ; exit 1 ; }
 mv -f "$1".tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
--- manuscript/resources/code/BashScripting/photo-backup-path.sh
+++ manuscript/resources/code/BashScripting/photo-backup-path.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bsdtar -cjf ~/photo.tar.bz2 ~/Documents/Photo && echo "bsdtar - OK" > results.txt || (echo "bsdtar - FAILS" > results.txt && exit 1)
+bsdtar -cjf ~/photo.tar.bz2 ~/Documents/Photo && echo "bsdtar - OK" > results.txt || { echo "bsdtar - FAILS" > results.txt ; exit 1 ; }
 cp -f ~/photo.tar.bz2 /d && echo "cp - OK" >> results.txt || ! echo "cp - FAILS" >> results.txt
