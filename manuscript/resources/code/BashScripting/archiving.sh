fe0ed34619f5f07d542332e9334b6ce8335d2882
Ilya Shpigor <petrsum@gmail.com>
2020-04-27 23:04:12 +0200

    Continue the "case operator" section

---
+++ manuscript/resources/code/BashScripting/archiving.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+operation="$1"
+
+if [[ "$operation" == "a" ]]
+then
+    bsdtar -c -f documents.tar ~/Documents
+elif [[ "$operation" == "c" ]]
+then
+    bsdtar -c -j -f documents.tar.bz2 ~/Documents
+elif [[ "$operation" == "x" ]]
+then
+    bsdtar -x -f documents.tar*
+else
+    echo "Указана недопустимая опция"
+fi
