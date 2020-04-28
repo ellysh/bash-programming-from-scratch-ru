78f4f87450c7320da72970163efa0a4f2a53b987
Ilya Shpigor <petrsum@gmail.com>
2020-04-28 21:24:10 +0200

    Continue the "case operator" section

---
+++ manuscript/resources/code/BashScripting/copy-archiving-duplication.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+operation="$1"
+
+case "$operation" in
+  "a")
+    find Documents -name "*.pdf" -type f -print0 | xargs -0 bsdtar -c -j -f documents.tar.bz2
+    cp documents.tar.bz2 ~/backup
+    ;;
+
+  "c")
+    cp documents.tar.bz2 ~/backup
+    ;;
+
+  *)
+    echo "Указана недопустимая опция"
+    ;;
+esac
---
+++ manuscript/resources/code/BashScripting/copy-archiving.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+operation="$1"
+
+case "$operation" in
+  "a")
+    find Documents -name "*.pdf" -type f -print0 | xargs -0 bsdtar -c -j -f documents.tar.bz2
+    ;&
+
+  "c")
+    cp documents.tar.bz2 ~/backup
+    ;;
+
+  *)
+    echo "Указана недопустимая опция"
+    ;;
+esac
