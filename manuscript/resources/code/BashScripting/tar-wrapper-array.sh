ed4d6799d7a8bad1961865ddf49176f1d76037ca
Ilya Shpigor <petrsum@gmail.com>
2020-04-29 21:24:05 +0200

    Complete the "case operator" section

--- manuscript/resources/code/BashScripting/tar-wrapper-array.sh
+++ manuscript/resources/code/BashScripting/tar-wrapper-array.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+option="$1"
 declare -A utils=(
   ["b"]="bsdtar"
   ["t"]="tar")
 
-${utils["$1"]} $2
+if [[ -z "$option" || ! -v utils["$option"] ]]
+then
+  echo "Указана недопустимая опция"
+  exit 1
+fi
+
+${utils["$option"]} $2
