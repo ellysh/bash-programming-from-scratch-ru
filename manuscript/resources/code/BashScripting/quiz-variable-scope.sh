ba4cfead9f6f55a8349ad7cebfec1226c3b7767a
Ilya Shpigor <petrsum@gmail.com>
2020-06-11 08:59:17 +0200

    Add an exercise for the scope of local variables

---
+++ manuscript/resources/code/BashScripting/quiz-variable-scope.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+bar()
+{
+    echo "bar1: var = $var"
+    var="bar_value"
+    echo "bar2: var = $var"
+}
+
+foo()
+{
+    local var="foo_value"
+
+    echo "foo1: var = $var"
+    bar
+    echo "foo2: var = $var"
+}
+
+echo "main1: var = $var"
+foo
+echo "main2: var = $var"
